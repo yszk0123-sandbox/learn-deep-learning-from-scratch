34051dce9e8840e3e7384220c5e26282d0b19bd9
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-02-07 09:55:36 +0900

    feat(ch03): add activation functions

--- examples/chapters/ch03/activation_functions.py
+++ examples/chapters/ch03/activation_functions.py
@@ -11,3 +11,14 @@ def step(x):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def softmax_naive(a):
+    exp_a = np.exp(a)
+    return exp_a / np.sum(exp_a)
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    return exp_a / np.sum(exp_a)
